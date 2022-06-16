26cf0ac5715d5d316f44e2195c318468da599b43
John Nwokocha <johnnwokocha@Johns-MacBook-Pro.local>
2022-06-16 11:21:53 +0100

    Task completed

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from cgitb import text
+
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.db import models
+from django.forms import CharField, DateTimeField
+
+User = get_user_model()
+
+# Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField(max_length=100)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE,)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
